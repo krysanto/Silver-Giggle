afd30378ad01c0595fec36c1a14f0b6ef5650722
Felix <51886988+Felix-beep@users.noreply.github.com>
2022-04-14 14:59:17 +0200

    Sprint 2 - WIP

---
+++ Assets/Scripts/Charaktere.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charaktere : MonoBehaviour
+{
+    public int Leben;
+    public GameObject Gegner;
+    public int Position;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Leben = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

7a604ec3cee6c44f4b6927dd93fcded89f1056c0
Felix <51886988+Felix-beep@users.noreply.github.com>
2022-05-02 15:37:35 +0200

    Added Buttons to make sprites appear
    Clicking the button now makes the old characters disappear and makes arbitrarely selected characters appear.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,16 +4,65 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public Character[] Characters;
+    public Transform CharacterFolder;
+    public List<Character> CharacterList = new List<Character>();
+    public int CharactersOwned = 0;
+    public List<Character> CharactersDisplayed = new List<Character>();
+
+    public GameObject BabyDragon;
+    public GameObject Dragon;
+    public GameObject Demon;
+    public GameObject Wraith;
 
     // Start is called before the first frame update
     void Start()
     {
-        Characters = FindObjectsOfType<Character>();
-        foreach(Character c in Characters)
+    }
+
+    public void AddCharacterToList(Character CharacterToAdd)
+    {
+        Debug.Log("Adding Character");
+        if(CharactersOwned+1 > 7) return;
+        CharacterList.Add(CharacterToAdd);
+        CharactersOwned++;
+        UpdateCharactersDisplayed();
+    }
+
+    public void MoveCharacter(int pos1, int pos2)
+    {
+        Character Temp = CharacterList[pos2];
+        CharacterList[pos2] = CharacterList[pos1];
+        CharacterList[pos1] = Temp;
+    }
+
+    public void RemoveCharacterFromList(Character CharacterToRemove)
+    {
+        if(CharacterList.Remove(CharacterToRemove));
         {
-            c.printName();
+            CharactersOwned--;
         }
+    }
+
+    public void PrintCharacter(Character CharacterToPrint, int x)
+    {
+        Instantiate(CharacterToPrint, new Vector3(x*2, 0, 0), Quaternion.identity, CharacterFolder); 
+    }
+
+    private void PrintCharacters()
+    {
+        GameObject[] toDestroy = GameObject.FindGameObjectsWithTag("Character");
+        foreach (GameObject i in toDestroy) Destroy(i.gameObject);
+        int counter = 0;
+        foreach (Character c in CharactersDisplayed) {
+            PrintCharacter(c, counter);
+            counter++;
+        }
+    }
+
+    private void UpdateCharactersDisplayed()
+    {
+        CharactersDisplayed = CharacterList;
+        PrintCharacters();
     }
 
     // Update is called once per frame

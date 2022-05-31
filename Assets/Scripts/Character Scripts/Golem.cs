1fc71f83240e6495c4d804b623015ef6f084f4a7
Korni9 <init0@gmx.at>
2022-05-31 12:06:44 +0200

    Anim_Scripte

---
+++ Assets/Scripts/Character Scripts/Golem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem: Character
+{
+    public override void ReduceHealth(int damage)
+    {
+        myAnimator.Play("Golem_Hurt");
+        base.ReduceHealth(damage);
+    }
+
+    public override bool Attack(Character Gegner)
+    {
+        myAnimator.Play("Golem_Attack");
+        return base.Attack(Gegner);
+    }
+
+    public override void Die()
+    {
+        myAnimator.Play("Golem_Death");
+        base.Die();
+    }
+
+    public override void Finished()
+    {
+        myAnimator.Play("Golem_Idle");
+        base.Finished();
+    }
+}
---
+++ Assets/Scripts/Character Scripts/Wraith.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wraith : Character
+{
+    public override void ReduceHealth(int damage)
+    {
+        myAnimator.Play("Wraith_Hurt");
+        base.ReduceHealth(damage);
+    }
+
+    public override bool Attack(Character Gegner)
+    {
+        myAnimator.Play("Wraith_Attack");
+        return base.Attack(Gegner);
+    }
+
+    public override void Die()
+    {
+        myAnimator.Play("Wraith_Death");
+        base.Die();
+    }
+
+    public override void Finished()
+    {
+        myAnimator.Play("Wraith_Idle");
+        base.Finished();
+    }
+}

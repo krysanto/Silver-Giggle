e3baf45a04667a69ea9cd998de1c7dd9447a83ee
Korni9 <init0@gmx.at>
2022-05-31 11:20:14 +0200

    Scripts and corrections

---
+++ Assets/Scripts/Character Scripts/Mage.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mage : Character
+{
+    public override void ReduceHealth(int damage)
+    {
+        myAnimator.Play("Mage_Hurt");
+        base.ReduceHealth(damage);
+    }
+
+    public override bool Attack(Character Gegner)
+    {
+        myAnimator.Play("Mage_Attack");
+        return base.Attack(Gegner);
+    }
+
+    public override void Die()
+    {
+        myAnimator.Play("Mage_Death");
+        base.Die();
+    }
+
+    public override void Finished()
+    {
+        myAnimator.Play("Mage_Idle");
+        base.Finished();
+    }
+
+    public override void DestroyMe()
+    {
+        base.DestroyMe();
+    }
+}
---
+++ Assets/Scripts/Character Scripts/Rogue.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rogue : Character
+{
+    public override void ReduceHealth(int damage)
+    {
+        myAnimator.Play("Rogue_Hurt");
+        base.ReduceHealth(damage);
+    }
+
+    public override bool Attack(Character Gegner)
+    {
+        myAnimator.Play("Rogue_Attack");
+        return base.Attack(Gegner);
+    }
+
+    public override void Die()
+    {
+        myAnimator.Play("Rogue_Death");
+        base.Die();
+    }
+
+    public override void Finished()
+    {
+        myAnimator.Play("Rogue_Idle");
+        base.Finished();
+    }
+
+    public override void DestroyMe()
+    {
+        base.DestroyMe();
+    }
+}
